71131baecab032234b4bc837bd01588c7e7a801e
hubforschoolwork <feliciaj6@sbcglobal.net>
2024-11-13 11:48:11 -0800

    code clean up

--- picker/admin.py
+++ picker/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from .models import AddPortfolio, DeletePortfolio, StockData, History
+# from .models import AddPortfolio, DeletePortfolio, StockData, History
 
 
 
-admin.site.register(AddPortfolio)
-admin.site.register(DeletePortfolio)
-admin.site.register(StockData)
-admin.site.register(History)
+# admin.site.register(AddPortfolio)
+# admin.site.register(DeletePortfolio)
+# admin.site.register(StockData)
+# admin.site.register(History)
 
 
 
